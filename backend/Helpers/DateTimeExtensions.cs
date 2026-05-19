f5be1af27e2ea1dd6e96b6a8bde6b17d851a4ca2
Oscar Rivas <oscar.rivas@yappa.dev>
2026-05-19 11:35:20 -0300

    feat(tarea002): aplicar ventana estricta 0..24h para ausencia/cancelacion

--- backend/Helpers/DateTimeExtensions.cs
+++ backend/Helpers/DateTimeExtensions.cs
@@ -2,8 +2,19 @@ namespace TurnosMedicos.Helpers;
 
 public static class DateTimeExtensions
 {
+    public static double HoursUntil(this DateTime fechaTurno)
+    {
+        return (fechaTurno - DateTime.Now).TotalHours;
+    }
+
     public static bool IsWithinCancellationWindow(this DateTime fechaTurno)
     {
-        return (fechaTurno - DateTime.Now).TotalHours <= 24;
+        var hoursUntil = fechaTurno.HoursUntil();
+        return hoursUntil >= 0 && hoursUntil <= 24;
+    }
+
+    public static bool IsExpired(this DateTime fechaTurno)
+    {
+        return fechaTurno.HoursUntil() < 0;
     }
 }
